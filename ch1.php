7b8059c82548665b99a9c78ae1a318d48d36c17a
lnwonder <93968966+lnwonder@users.noreply.github.com>
2022-01-02 13:44:11 +0800

    Add files via upload

---
+++ ch1.php
@@ -0,0 +1,345 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	
+	<title>The Hellblaze LN | Wizard Fate I — Chapter 1</title >
+
+<meta name="description" content="In a place where lush green stretches to the horizons, the wilderness pretends to be peaceful. However, this peacefulness is often chased away by loud roars of unknown beasts. The place was deadly and cruel. "  />
+
+	<link rel="icon" href="https://thehellblaze.000webhostapp.com/logo.png" />
+  <link rel="stylesheet" href="abc123header.css" type="text/css"/>
+  <link rel="stylesheet" href="abc123body.css" type="text/css"/>
+<link rel="stylesheet" href="abc123navigator.css" type="text/css"/>
+</head>
+<body>
+
+
+    <div id="header">
+      <div id="c1">
+      <img id="logo" src="https://thehellblaze.000webhostapp.com/logo.png">
+        </div>
+      <div>
+      <h3>The Hellblaze Novels</h3>
+       </div>
+    </div>
+<br>
+
+
+
+
+
+<!--novel navigator--->
+
+<div id="navi3">
+<li>
+ <ul><a id="h" href="https://thehellblaze.000webhostapp.com/">Home</a></ul>
+     <ul><a id="n" href="https://thehellblaze.000webhostapp.com/novels/">⟨ Novels</a></ul>
+<ul><a id="ct" href="https://thehellblaze.000webhostapp.com/wizard_fate_1/">⟨ Chapters</a></ul>
+    </li>
+  </div>
+
+
+
+
+<br>
+ <br>
+
+
+  <!--this part is for the next and prev button for navigation through chapters. Edit for every update-->
+  <div class="chap_navi">
+    <a href="https://thehellblaze.000webhostapp.com/novels/wizard_fate_1/chapters/" class="btn">Prev</a>
+  <a href="https://thehellblaze.000webhostapp.com/novels/wizard_fate_1/chapters/ch2.php" class="btn">Next</a>
+    </div>
+
+
+
+
+
+
+<!--Novel part contents goes here-->
+  <div id="main">
+
+
+
+   <!--title of the part goes here-->
+<div id="ch">
+    <h3 id="part_title" >Chapter 1</h3>
+    </div>
+
+<br>
+   
+    <br>
+    <br>
+   <!--the chapter contents goes here paragraph by paragraph-->
+     <div id="part_contents">
+
+       <p1>
+  
+In a place where lush green stretches to the horizons, the wilderness pretends to be peaceful. However, this peacefulness is often chased away by loud roars of unknown beasts. The place was deadly and cruel.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>But there was also a paradise. Between three mountains, there was a valley of tall grasses a few kilometers from the edge of the woods. In the middle, a lonely old wooden house stood.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>From far away, the wooden house may seem abandoned but there were actually thin smokes seeping out of the narrow slits on the wooden walls, a hint that there was someone in the house.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>It was a boy, about fifteen of age. But he wasn't just any boy, he looked matured like a real man. He had a slender figure, wearing shabby black clothes. His black hair was long as though it hasn't been cut or trimmed for a few couples of months. However it was tidied, making him appear handsome.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>At the moment, he was looking curiously at a small bottle, about the size of three thumbs, containing a slimy green liquid. It was yesterday's harvest from a filthy crawling beast that almost ended his life. Not very rare, he shortly put it back to his small leather bag which he always carries on his waist.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>Suddenly, footsteps sounded from outside the house, just near the entrance door which was closed. Hearing it, his head turned towards the wooden door half as fast as lightning. At the same time, his hands quickly took the sword from the wooden table, one on the fancy scabbard and one on the hilt.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>~Knock ~Knock ~Knock.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>The door quickly opened even without the his words. It made him draw the edge of sword. But then, he halted his sword's movement halfway out of the scabbard when a familiar tall figure of a man came to his sight.</p1><br>
+<br>
+<p1>The man wore a black robe and also held a fancy sword. There was also an iron badge on his chest, depicting a vertical sword with bright sun behind its hilt. His appearance was like a mercenary.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Mariko?" The boy's eye brows raised a little and he returned his sword back into the scabbard. He didn't expect the man to come. But shortly, he recovered and his face turned solemn as though he knew something.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"You've come to bring me back... again."</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>Hearing him, the tall guy named "Mariko" couldn't help but sigh. He has been ordered to bring the youth in front of him back to the city for the third time. All his previous attempts failed because of the youth's stubbornness.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"As expected," the boy said. He had guessed correctly.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"But you have to come with me this time, Langris." Mariko was serious as he glanced straight to the eyes of the youth who he called "Langris."</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"That ain't happening," Langris simply replied.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"The House already know. Uncle Theos has been demoted from the Valarian House Head's position."</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"What?!" Hearing Mariko's explanation, Langris exclaimed angrily. He couldn't help but gnash his teeth and clench his fists. "Why'd they demoted my father from his position?"</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"The First Elder and Second Elder of the House claimed that the wizard bloodline in your father has weakened, therefore producing you, a non-wizard," explained Mariko. As he did, he couldn't help but feel pity towards Langris in his heart.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>Langris was a hard-working boy and worth the praise. But it was a cruel fate for him as a non-wizard to be born in a wizarding family. What was worse is that, he was supposed to be the next Head of the House.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>Unable to suppress his anger to himself and the house, he faced the ceiling and placed his palms on his eyes. He was pitiful. Tears could fall out of his eyes anytime now, unable to bear his suffering. But he forced to stop them, turning his eyes and face red.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Your father asked me to bring you back. If we leave now, we might reach Stonewill tomorrow before dark."</p1><br>
+<br>
+<p1>Langris stayed silent as though he didn't hear Mariko. After a few seconds, he still showed no sign of recovering and his knees grew weak. Shortly, he sank back on the chair lifelessly.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>For more than a minute, tranquility dominated the room. Seeing the weak boy who seemed to have been struck by intense headache, Mariko did not say a word. He only watched Langris as there was nothing he could do.</p1><br>
+<br>
+<p1>Shortly, Langris finally tried to calm himself. His fingers which has dug deep into his palms gradually relaxed. After gritting his teeth, he couldn't only heave a deep sigh. No matter how he hate himself and his fate, it's not going to solve anything.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>Langris took his sword from the table. Seeing his action, Mariko eschewed explaining and said, "let's go."</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>The journey back to Stonewill took them a day and a half; they weren't really on a hurry. And as they traveled, Langris' mood has also gradually went a little better. They passed through the woods and encountered a few beasts in its wideness but not pricks on the eyes. Then they traveled through a wide grassland. When the sun went ahead of them for the second time, it was when a distant giant city grew in their sight.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>Stonewill City...</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>Near the edge of the wide grassland, a prosperous city stood mightily. It was surrounded by thick and tall walls of concrete, forming a gigantic octagon when viewed from the clouds. But these walls showed noticeable signs of deterioration and big marks like scars in all of sides: Those marks all came from deadly giant beasts attacking the city in the past few centuries, one to three times every five years.  With only a glance, one could say that if the city wasn't mighty, it wouldn't have lasted for a millennia.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>The city has long been here but only five towering structures stood in it. They showed the number of forces protecting it: The City Hall at the center, and one of the Four Great Houses in each of the four directions (North, South, East and West).</p1><br>
+<br>
+<p1>It has already been eight months since Langris left this Stonewill City. Reaching the huge entrance gate of the city which was heavily guarded, he momentarily halted his steps and glanced towards the boisterous part of the city, through the gaps of the woven metal bars. As he was in his thoughts, Mariko behind him showed the badge on his chest to the guards. Shortly, the metal door sounded as it moved upwards, pulled by hard metal chains, opening the gate.</p1><br>
+<br>
+<p1>Langris let out a mouthful of air before he continued walking, going towards the inner part of the city filled with people. Going back to this city so soon wasn't his plan; he's still unprepared to face the family.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Golden Grass here! Trading for two Common-grade Magic Relics!"</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Water Beast Amulet! This can attract beasts that possess water element! Selling for eleven gold coins."</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Earth Origin Nut! Earth Origin Nut! Trading for three Spiritual Pellets."</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>★★★</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>Langris hasn't yet taken a long stroll in the city since he entered. This time, he's at the most boisterous, lively, and interesting part of the village, the "Rare Market," which is close to the entrance and exit of the city. It is called so simply because it is a trade area where people, mostly explorers, wizards and alchemists, meet to buy or sell rare items. However this time, he didn't seem to be in the right mood to put much of his attention in this place.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>After a few minutes of walking on the road, he finally arrived at the inner part of the city. It was more peaceful than the outer part, at least, and he was used to this place since he was younger. Looking at it, there seemed to be no significant changes since he left.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Now that you're here again, what are your plans?" </p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>Finally, after the long silence between the two of them, Mariko has spoken from behind. </p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"You must survive, at least. But giving up is not an option, I won't advise you that."</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>Hearing Mariko, Langris only glanced at him for a short moment before randomly looking around.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"I don't know," his step shortly came to stop before he said in his shallow voice and uncheerful tone.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>Just when he was looking around, a man in thirties who was secretly placing an apple above an apple fruit stand came to his sight. In the first second he saw it, he already knew that the man's action was rather suspicious. He had seen such familiar tactic many times before.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>Behind, Mariko was clueless about what Langris was thinking. All he knew was that Langris does not talk a lot when his mind is busy. Shortly, when he could no longer bear the silence of the latter, he asked him, "hey, why don't you go and see your father first? He's been worried about you all this time."</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Em." Langris nodded after the suggestion without turning his head towards Mariko. He then walked towards the apple fruit stand while his glance locked at the apple which the suspicious man placed before. Stopping at the front, he looked at the owner of the fruit stand who was an old woman on her seventies. When he found out that the apple was actually rotten on one side, he felt pity towards the old woman.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Grandma, there's a rotten apple in your fruit stand," Langris said and took the apple. The apple had a dark spot in it which has some molds.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Take a look."</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Oh! How come there's a rotten apple here? I've carefully checked all of these before I displayed them here," the old woman was surprised. "Thank you for telling me."</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Uhm, Grandma, can I have this rotten apple for free? I'll feed it to my hungry pet at home," Langris lied.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Won't your pet get sick?"</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Nope, he's a weird one," laughed Langris.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Hahaha, oh yes, take it! It's even better when wastes aren't garbage," the old woman also laughed and took a fresh apple. "Here is a fresh one, take it too. Good deeds and kindness are rewarded, ya know?"</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Thank you, Grandma. I'll be going." Langris smiled.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>After he left the fruit stand, he took a glance at the rotten apple before looking around to find the suspicious man. Not long after, a grin showed on his face when the man came to his sight again. The man was actually also a fruit vendor who sells apples and oranges.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Sir, how much is your apple here? Langris inquired at the man after approaching the fruit stand.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Three coppers for one apple," answered the man as he raised three fingers with a smile. </p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"This is already the best price. Moreover, my apples here are all fresh."</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>Hearing the man, Langris smiled. He then took an apple from the pile. Seeing his action, traces of excitement surfaced to on the vendor's face. But Langris only laughed inwardly at the vendor.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Sir, are you trying to fool us all here? Three coppers for a rotten apple?!" Langris' face turned dark as he raised his voice. He grinned. When the other customers heard him, they all took a glance at the apple in his hand; it was indeed rotten.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"What? Impossible!" The vendor trembled  in shock. His face gradually turned slightly gloomy, feeling embarrassed about the words he said earlier.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>Because of what happened, the other customers started to be doubtful towards the fresh appearances of the apples they purchased. Some of them returned the apples and took back their money. The man initially didn't agree with them but unfortunately, one of the customers who complained turned out to be a wizard and threatened him with an offensive magic spell. </p1><br>
+<br>
+<p1>Meanwhile, Langris had already taken several steps away from the man's fruit stand.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Your hands are really quick as always," laughed Mariko.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>"Nah, just a simple trick. It doesn't work in front of a genuine wizard," replied Langris disappointedly as he took an apple from his leather bag. After that, he already have two fresh apples: the one which the old woman gave and another which he stole from the other vendor by secretly swapping it with the rotten one. Taking a bite from from one of the fresh apples, he tossed the other one to Mariko.</p1><br>
+<br>
+<p1></p1><br>
+<br>
+<p1>
+         </p1>
+
+     </div>
+
+
+   </div>
+
+<!--this part is for the next and prev button for navigation through chapters. Edit for every update-->
+  <div class="chap_navi">
+    <a href="https://thehellblaze.000webhostapp.com/novels/wizard_fate_1/chapters/" class="btn">Prev</a>
+  <a href="https://thehellblaze.000webhostapp.com/novels/wizard_fate_1/chapters/ch2.php" class="btn">Next</a>
+    </div>
+
+
+
+
+
+
+
+
+  </body>
+  </html>
